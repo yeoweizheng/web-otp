f2e3ce310e35bacb410e0a7193c63c22fcf12831
Wei Zheng <wzyeo1990@gmail.com>
2024-09-03 22:02:18 +1000

    test packaging

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
-import ("fmt")
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
-  fmt.Println("Hello World!")
+	Test()
+	// createUserSQL := `CREATE TABLE users ("id" integer PRIMARY KEY AUTOINCREMENT, "username" TEXT, "password" TEXT);`
+	// statement, _ := db.Prepare(createUserSQL)
+	// statement.Exec()
+	// defer db.Close()
+
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run(":9000")
 }
