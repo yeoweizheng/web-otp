3e22bdf955e22f9834bcec2b09c5d299d79e908e
Wei Zheng <wzyeo1990@gmail.com>
2024-09-15 01:28:16 +1000

    bugfix

--- static/js/index.js
+++ static/js/index.js
@@ -35,7 +35,7 @@ async function refreshAccountOTPs() {
 
 function updateTable() {
     let html = "";
-    if (currTableData.length == 0) return;
+    if (!currTableData) return;
     currTableData.sort((a, b) => a.name.localeCompare(b.name))
     let searchText = $("#searchInput").val().toLowerCase()
     for (let row of currTableData) {
